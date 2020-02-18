863af3f8cea639c987f72568c141a29fc6a1e2c7
Jesper Svensson <jessve-5@student.ltu.se>
2020-02-18 19:46:41 +0100

    knapsack without table

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-f = open("inputs/a_example.in", "r")
+
+def knapsack(n, numbers):
+    if n <= 0:
+        return 0
+    elif len(numbers) == 0:
+        return 0
+    elif n - numbers[0] < 0:
+        return knapsack(n, numbers[1:])
+    else:
+     return max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
+
+filename = "c_medium.in"
+f = open("inputs/"+filename, "r")
 data = f.readline().split(" ")
-M = data[0]
+M = int(data[0])
 N = data[1]
-pizzas = map(lambda x: int(x), f.readline().split(" "))
-print(list(pizzas))
+pizzas = [int(x) for x in f.readline().split(" ")]
+print("max:", M)
+print(knapsack(M, pizzas))
+
