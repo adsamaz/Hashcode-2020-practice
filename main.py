b31358bd6f56e2ecf1201499b15bd61e705ef223
Jesper Svensson <jessve-5@student.ltu.se>
2020-02-18 20:10:08 +0100

    add table

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import time
 
 def knapsack(n, numbers):
+    l = len(numbers)
     if n <= 0:
+        T[n][l] = 0
         return 0
     elif len(numbers) == 0:
+        T[n][l] = 0
         return 0
     elif n - numbers[0] < 0:
-        return knapsack(n, numbers[1:])
+        if T[n][l] == 0:
+            T[n][l] = knapsack(n, numbers[1:])
     else:
-        return max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
+        if T[n][l] == 0:
+            T[n][l] = max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
+    return T[n][l]
+
 
 filename = "c_medium.in"
 f = open("inputs/"+filename, "r")
@@ -15,6 +23,13 @@ data = f.readline().split(" ")
 M = int(data[0])
 N = int(data[1])
 pizzas = [int(x) for x in f.readline().split(" ")]
-print("max:", M, "number of pizzas:", N)
-print(knapsack(M, pizzas))
 
+T = [[0 for _ in range(len(pizzas) + 1)] for _ in range(M+1)]
+
+print("max:", M, "number of pizzas:", N)
+
+
+start = time.time()
+print(knapsack(M, pizzas))
+end = time.time()
+print("Running time:", end - start)
