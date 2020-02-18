ab961b13858cd9adf2a9b1f539b00afe31bd5406
Anders Lagerkvist <anders.lagerkvist2@ibm.com>
2020-02-18 19:21:29 +0100

    read file

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
- f = open("inputs/a_example.in", "r")
- print(f.readline())
+f = open("inputs/a_example.in", "r")
+data = f.readline().split(" ")
+M = data[0]
+N = data[1]
+pizzas = map(lambda x: int(x), f.readline().split(" "))
+print(list(pizzas))
