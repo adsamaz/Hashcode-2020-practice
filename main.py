8dc410d95f45ab36c2d849fbf2915bdd446bd475
Jesper Svensson <jessve-5@student.ltu.se>
2020-02-18 20:35:22 +0100

    Impl. dict (hash-map) instead of static list

--- main.py
+++ main.py
@@ -3,31 +3,32 @@ import time
 def knapsack(n, numbers):
     l = len(numbers)
     if n <= 0:
-        T[n][l] = 0
+        T[(n,l)] = 0
         return 0
     elif len(numbers) == 0:
-        T[n][l] = 0
+        T[(n,l)] = 0
         return 0
     elif n - numbers[0] < 0:
-        if T[n][l] == 0:
-            T[n][l] = knapsack(n, numbers[1:])
+        if not T.get((n,l)):
+            T[(n,l)] = knapsack(n, numbers[1:])
     else:
-        if T[n][l] == 0:
-            T[n][l] = max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
-    return T[n][l]
+        if not T.get((n,l)):
+            T[(n,l)] = max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
+    return T[(n,l)]
 
 
 filename = "c_medium.in"
 f = open("inputs/"+filename, "r")
+print("reading file...")
 data = f.readline().split(" ")
 M = int(data[0])
 N = int(data[1])
 pizzas = [int(x) for x in f.readline().split(" ")]
-
-T = [[0 for _ in range(len(pizzas) + 1)] for _ in range(M+1)]
-
+print("creating data structure...")
+T = {}
+print("done.")
 print("max:", M, "number of pizzas:", N)
-
+print("calculating...")
 
 start = time.time()
 print(knapsack(M, pizzas))
