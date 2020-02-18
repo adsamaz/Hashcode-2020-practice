6c94670809b331a115f3d6972ef75caff8da29e0
Jesper Svensson <jessve-5@student.ltu.se>
2020-02-18 19:51:40 +0100

    fix indentation. Start imp table

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def knapsack(n, numbers):
     elif n - numbers[0] < 0:
         return knapsack(n, numbers[1:])
     else:
-     return max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
+        return max(knapsack(n - numbers[0], numbers[1:]) + numbers[0], knapsack(n, numbers[1:]))
 
 filename = "c_medium.in"
 f = open("inputs/"+filename, "r")
 data = f.readline().split(" ")
 M = int(data[0])
-N = data[1]
+N = int(data[1])
 pizzas = [int(x) for x in f.readline().split(" ")]
-print("max:", M)
+print("max:", M, "number of pizzas:", N)
 print(knapsack(M, pizzas))
 
